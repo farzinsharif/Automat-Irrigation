892c37fa9222db2be694611edf178cd8cd0b71fa
farzinsharif <farzinsharif.j@gmail.com>
2024-07-08 15:30:45 +0330

    proper format after if condition

--- app.py
+++ app.py
@@ -3,108 +3,36 @@ app = Flask(__name__)
 
 posts = [
     {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
+        'date': '15 July',
+        'entries': [
+            {'time': '15:30', 'moisture_level': '1798'},
+            {'time': '16:00', 'moisture_level': '1800'}
+        ]
     },
     {
-        'date': '22 march',
-        'time': '23:47',
-        'moisture_level': '3215',
-        'time': '23:47',
-        'moisture_level': '3215',
-        'time': '23:47',
-        'moisture_level': '3215',
-        'time': '23:47',
-        'moisture_level': '3215',
-
-
+        'date': '22 March',
+        'entries': [
+            {'time': '23:47', 'moisture_level': '3215'},
+            {'time': '00:00', 'moisture_level': '3220'},
+            {'time': '15:30', 'moisture_level': '1798'},
+            {'time': '16:00', 'moisture_level': '1800'}
+        ]
     },
     {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
+        'date': '30 بهمن',
+        'entries': [
+            {'time': '23:47', 'moisture_level': '3215'},
+            {'time': '00:00', 'moisture_level': '3220'}
+        ]
     },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
+{
+        'date': '1 august',
+        'entries': [
+            {'time': '23:47', 'moisture_level': '3215'},
+            {'time': '00:00', 'moisture_level': '3220'}
+        ]
     },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
 
-    },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
-    },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
-    },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
-    },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
-    },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
-    },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
-    },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
-    },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
-    },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
-    },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
-    },
-    {
-        'date': '15 july',
-        'time': '15:30',
-        'moisture_level': '1798'
-
-    },
 ]
 
 
